c79cd15e2a07483766a2e643cd1c6b5cbe034910
Dinesh Kumar <dkdineshkumar2001@gmail.com>
2026-03-29 23:32:10 +0530

    Prisma FIxes Final

---
+++ prisma/migrations/20260329175543_column_a_deleted_in_test_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `field_a` on the `test_zithspace` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "test_zithspace" DROP COLUMN "field_a";
