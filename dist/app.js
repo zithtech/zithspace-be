743d5cf69ec205bca2d791a7639f783635649aa6
owner-zithtech <owner@zithtech.com>
2026-06-26 16:16:16 +0530

    Rabbit Mq issue

--- dist/app.js
+++ dist/app.js
@@ -129,6 +129,8 @@ const candidateRoutes_1 = __importDefault(require("@/routes/candidateRoutes"));
 const companyLocationRoutes_1 = __importDefault(require("@/routes/companyLocationRoutes"));
 const openingManagementRoutes_1 = __importDefault(require("@/routes/openingManagementRoutes"));
 const RabbitMQService_1 = require("@/utils/RabbitMQService");
+const CalendarSyncWorker_1 = require("@/workers/CalendarSyncWorker");
+const MailSyncWorker_1 = require("@/workers/MailSyncWorker");
 //import { CentralMailWorker } from "@/workers/CentralMailWorker";
 const MailController_1 = require("@/controllers/MailController");
 const lead_routes_1 = __importDefault(require("@/routes/lead.routes"));
@@ -526,9 +528,9 @@ const startServer = async () => {
         await ensureOnboardingSchema();
         // Connect RabbitMQ & Start Workers
         try {
-            // await rabbitMQService.connect();
-            // await CalendarSyncWorker.start();
-            // await MailSyncWorker.start();
+            await RabbitMQService_1.rabbitMQService.connect();
+            await CalendarSyncWorker_1.CalendarSyncWorker.start();
+            await MailSyncWorker_1.MailSyncWorker.start();
             // await CentralMailWorker.start();
             console.log("🚀 RabbitMQ connected, Calendar & Mail Sync Workers started");
         }
